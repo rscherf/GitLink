76e0eb635470d0943416bc12d6f39214597eabdd
Ryan Scherf <scherf.ryan@gmail.com>
2014-10-16 12:25:12 -0500

    Perfect path

---
+++ Gitlink.py
@@ -0,0 +1,40 @@
+import os, commands, difflib, re, webbrowser, sublime, sublime_plugin
+
+class GitlinkCommand(sublime_plugin.TextCommand):
+  def run(self, edit, **args):
+    # Current file path & filename
+    path, filename = os.path.split(self.view.file_name())
+
+    # Switch to cwd of file
+    os.chdir(path + "/")
+
+    # Find the repo
+    git_config_path = commands.getoutput("git remote show origin")
+    p = re.compile(r"(.+@)*([\w\d\.]+):(.*)")
+    git_config = p.findall(git_config_path)[0][2]
+
+    # Get Github username and repository
+    user, repo = git_config.replace(".git", "").split("/")
+
+    # Find top level repo in current dir structure
+    basename = commands.getoutput("basename `git rev-parse --show-toplevel`")
+    remote_path = path.split(basename)[1]
+
+    # Find the current branch
+    branch = commands.getoutput("git rev-parse --abbrev-ref HEAD")
+
+    # Build the URL
+    url = "https://github.com/{0}/{1}/blob/{2}{3}/{4}".format(user, repo, branch, remote_path, filename)
+
+    if(args['line']):
+      row =  self.view.rowcol(self.view.sel()[0].begin())[0] + 1
+      url += "#L{0}".format(row)
+
+    if(args['web']):
+      webbrowser.open_new_tab(url)
+      print "Opened '" + url + "' in a new tab."
+    else:
+      print "Copied '" + url + "' to clipboard."
+      os.system("echo '%s' | pbcopy" % url)
+
+
---
+++ README.md
@@ -0,0 +1,19 @@
+# Gitlink
+
+Sublime Text 2 plugin to derive <http://github.com> URLs to files in your project. No more traversing your file structure finding the file you are working on.
+
+## How it works
+
+
+
+#### Copy URLs to files
+
+Right click any file in the sidebar (that is part of a Github.com repository) and go to the `Gitlink` menu item to see options. 
+
+#### Copy URLs to files with a deeplink the line number
+
+Right click anywhere within the file you are currently editing. Your cursor position determines which line number will be used for the deeplink.
+
+#### Open Github.com URLs in your default browser as a new tab
+
+Use your default web browser (Chrome) to skip a step and open any of the links automatically in a new tab.
