942bcb23a1573ac6cec4dff40c6d326b46bdc620
John Hu <hszqqq12@gmail.com>
2021-06-05 13:17:02 -0700

    Prioritize upstream Git URL

--- GitLink.py
+++ GitLink.py
@@ -5,33 +5,28 @@ import sublime
 import sublime_plugin
 import subprocess
 
-REMOTE_CONFIG = {
+HOSTINGS = {
     'github': {
-        'url': 'https://github.com/{0}/{1}/blob/{2}/{3}{4}',
-        'blame_url': 'https://github.com/{0}/{1}/blame/{2}/{3}{4}',
+        'url': 'https://github.com/{user}/{repo}/blob/{revision}/{remote_path}{filename}',
+        'blame_url': 'https://github.com/{user}/{repo}/blame/{revision}/{remote_path}{filename}',
         'line_param': '#L',
         'line_param_sep': '-L'
     },
     'bitbucket': {
-        'url': 'https://bitbucket.org/{0}/{1}/src/{2}/{3}{4}',
-        'blame_url': 'https://bitbucket.org/{0}/{1}/annotate/{2}/{3}{4}',
+        'url': 'https://bitbucket.org/{user}/{repo}/src/{revision}/{remote_path}{filename}',
+        'blame_url': 'https://bitbucket.org/{user}/{repo}/annotate/{revision}/{remote_path}{filename}',
         'line_param': '#cl-',
         'line_param_sep': ':'
     },
-    'codebasehq': {
-        'url': 'https://{0}.codebasehq.com/projects/{1}/repositories/{2}/blob/{3}{4}/{5}',
-        'blame_url': 'https://{0}.codebasehq.com/projects/{1}/repositories/{2}/blame/{3}{4}/{5}',
-        'line_param': '#L',
-        'line_param_sep': ':'
-    },
     'gitlab': {
-        'url': 'https://{0}/{1}/{2}/-/blob/{3}/{4}{5}',
-        'blame_url': 'https://{0}/{1}/{2}/-/blame/{3}/{4}{5}',
+        'url': 'https://{domain}/{user}/{repo}/-/blob/{revision}/{remote_path}{filename}',
+        'blame_url': 'https://{domain}/{user}/{repo}/-/blame/{revision}/{remote_path}{filename}',
         'line_param': '#L',
         'line_param_sep': '-'
     }
 }
 
+REMOTE_URL_PATTERN = re.compile(r"Fetch URL: [\w\d\.]+[:|@]/?/?(.*)")
 
 class GitlinkCommand(sublime_plugin.TextCommand):
 
@@ -48,82 +43,56 @@ class GitlinkCommand(sublime_plugin.TextCommand):
         # Switch to cwd of file
         os.chdir(path + "/")
 
-        # Find the repo
-        git_config_path = self.getoutput("git remote show origin -n")
-
+        # Find the remote
+        remote_name = self.getoutput("git remote show | grep upstream || echo origin")
+        fetch_url = self.getoutput(f"git remote show {remote_name} -n | grep 'Fetch URL: '")
         # Determine git URL which may be either HTTPS or SSH form
         # (i.e. https://domain/user/repo or git@domain:user/repo)
         #
-        # parts[0][2] will contain 'domain/user/repo' or 'domain:user/repo'
-        #
-        # see https://regex101.com/r/pZ3tN3/2 & https://regex101.com/r/iS5tQ4/2
-        p = re.compile(r"(.+: )*([\w\d\.]+)[:|@]/?/?(.*)")
-        parts = p.findall(git_config_path)
-        git_config = parts[0][2]
+        # 'remote' would be 'domain/user/repo' or 'domain:user/repo'
+        remote = REMOTE_URL_PATTERN.search(fetch_url).group(1)
+        remote = re.sub('.git$', '', remote)
 
-        remote_name = 'github'
-        if 'bitbucket' in git_config:
-            remote_name = 'bitbucket'
-        if 'codebasehq.com' in git_config:
-            remote_name = 'codebasehq'
-        if 'gitlab' in git_config:
-            remote_name = 'gitlab'
-        remote = REMOTE_CONFIG[remote_name]
+        for hosting_name, hosting in HOSTINGS.items():
+            if hosting_name in remote:
+                break
 
-
-        # need to get username from full url
-
-        # Get username and repository (& also project for codebasehq)
-        remote_without_git = re.sub('.git$', '', git_config)
-        if ':' in git_config:
+        # Get username and repository
+        if ':' in remote:
             # SSH repository
-            if remote_name == 'codebasehq':
-                # format is codebasehq.com:{user}/{project}/{repo}.git
-                domain, user, project, repo = remote_without_git.replace(":", "/").split("/")
-            else:
-                # format is {domain}:{user}/{repo}.git
-                domain, user, repo = remote_without_git.replace(":", "/").split("/")
+            # format is {domain}:{user}/{repo}.git
+            domain, user, repo = remote.replace(":", "/").split("/")
         else:
             # HTTP repository
-            if remote_name == 'codebasehq':
-                # format is {user}.codebasehq.com/{project}/{repo}.git
-                domain, project, repo = remote_without_git.split("/")
-                user = domain.split('.', 1)[0] # user is first segment of domain
-            else:
-                # format is {domain}/{user}/{repo}.git
-                domain, user, repo = remote_without_git.split("/")
+            # format is {domain}/{user}/{repo}.git
+            domain, user, repo = remote.split("/")
 
         # Find top level repo in current dir structure
         remote_path = self.getoutput("git rev-parse --show-prefix")
 
         # Find the current revision
-        revsion = self.getoutput("git rev-parse HEAD")
+        revision = self.getoutput("git rev-parse HEAD")
 
         # Choose the view type we'll use
-        if('blame' in args and args['blame']):
+        if 'blame' in args and args['blame']:
             view_type = 'blame_url'
         else:
             view_type = 'url'
 
         # Build the URL
-        if remote_name == 'codebasehq':
-            url = remote[view_type].format(user, project, repo, revsion, remote_path, filename)
-        elif remote_name == 'gitlab':
-            url = remote[view_type].format(domain, user, repo, revsion, remote_path, filename)
-        else:
-            url = remote[view_type].format(user, repo, revsion, remote_path, filename)
+        url = hosting[view_type].format(domain=domain, user=user, repo=repo, revision=revision, remote_path=remote_path, filename=filename)
 
-        if(args['line']):
+        if args['line']:
             region = self.view.sel()[0]
             first_line = self.view.rowcol(region.begin())[0] + 1
             last_line = self.view.rowcol(region.end())[0] + 1
             if first_line == last_line:
-                url += "{0}{1}".format(remote['line_param'], first_line)
+                url += "{0}{1}".format(hosting['line_param'], first_line)
             else:
-                url += "{0}{1}{2}{3}".format(remote['line_param'], first_line, remote['line_param_sep'], last_line)
+                url += "{0}{1}{2}{3}".format(hosting['line_param'], first_line, hosting['line_param_sep'], last_line)
 
-        if(args['web']):
+        if args['web']:
             webbrowser.open_new_tab(url)
         else:
             sublime.set_clipboard(url)
-            sublime.status_message('GIT url has been copied to clipboard')
+            sublime.status_message('Git URL has been copied to clipboard')
