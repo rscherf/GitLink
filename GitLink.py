56d677e62d7776084b65c4f766e2cc355aeb9c16
Michael Lyons <github@michael.lyo.nz>
2025-12-13 19:11:51 -0500

    Move settings to a settings file

--- GitLink.py
+++ GitLink.py
@@ -34,8 +34,8 @@ class GitlinkCommand(sublime_plugin.TextCommand):
         os.chdir(path + "/")
 
         # Find the current revision
-        settings = sublime.load_settings("Preferences.sublime-settings")
-        ref_type = settings.get('gitlink_revision_type', 'abbrev')
+        settings = sublime.load_settings('GitLink.sublime-settings')
+        ref_type = settings.get('revision_type')
         if ref_type == 'commithash':
             revision = self.getoutput("git rev-parse HEAD")
         elif ref_type == 'abbrev':
